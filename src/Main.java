1fbe499373a54433a7d70fbf21f8937374a7c0da
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-11-13 14:56:36 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите год начала жизни: ");
+        }
+}
